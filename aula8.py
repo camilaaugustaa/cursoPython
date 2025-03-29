d6fc98bbff08588f87f5e59b9ea4e0b63a79b9b3
camilaaugustaa <camilaaugustasnt@outlook.com>
2025-03-29 17:32:37 -0300

    exercício aula 8

--- aula8.py
+++ aula8.py
@@ -23,10 +23,42 @@ def calcular_bonus(lista_vendas):
 vendas = [100 ,200, 4890, 1000, 500]
 
 resultado = calcular_bonus(vendas)
-bonus1 = resultado[0] #tupla indice 0 bonus1
-bonus2 = resultado[1] #tupla indice 1 bonus2. Essa forma mostra o valor de cada um dos bônus separadamente
-print(resultado)
+bonus1, bonus2 = calcular_bonus(vendas) #Só vai funcionar se a função me devolve dois valores como resposta
+#unpacking da tupla, atribuindo os valores retornados da função calcular_bonus para as variaveis bonus1 e bonus2
 print(bonus1)
 print(bonus2)
 
 
+
+#bonus 1: R$2 POR VENDA FEITA
+#bonus 2: 1% DO VALOR DE VENDAS
+
+def calcular_bonus(lista_vendas):
+    bonus1 = 2 * len(lista_vendas) # R$2 por venda da lista
+    bonus2 = 0.01 * sum(lista_vendas) # 1% do valor total de vendas
+    return bonus1, bonus2
+
+vendas = {
+    "André": [1000, 500, 300, 5000, 1500, 80, 3000],
+    "Andressa": [1500, 9000, 300, 150, 1500, 120, 130, 55, 500, 8500],
+    "Alan": [800, 100],
+    "Ana": [800, 900, 950, 1200, 1600, 130, 50, 50, 50, 50, 65, 60, 70, 70, 70, 200, 180, 100, 120, 110, 130, 140]
+}
+
+#bonus de cada funcionario
+# total de bonus 1 pago aos funcionarios
+# total de bonus 2 pago aos funcionarios
+
+total_bonus1 = 0
+total_bonus2 = 0
+for vendedor in vendas:
+    bonus1, bonus2 = calcular_bonus(vendas[vendedor])
+    print(f"Vendedor {vendedor}. Bonus 1: {bonus1}. Bonus 2: {bonus2}.")
+    total_bonus1 = total_bonus1 + bonus1
+    total_bonus2 = total_bonus2 + bonus2
+    
+print("Total Bonus 1:", total_bonus1)
+print("Total Bonus 2:", total_bonus2)
+print("Total Bonus geral:", total_bonus1 + total_bonus2)
+
+    
